d7eb689de43cf41af9ee7e3c5f36076b607d9303
unknown <you@example.com>
2022-02-14 10:55:33 +0100

    Developer skills. Start.

---
+++ 03-Developer-Skills/script.js
@@ -0,0 +1,7 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// install VSCode extension Prettier - Code firmatter
+//File-Settings-Editoor: Default formatter - choose Prettier
+const x=23;
+
