ae2c58f24d056792ca386014fc43e8c9d6cde329
unknown <you@example.com>
2022-02-15 22:10:08 +0100

    Modal window. Done.

--- 04-Browser DOM Events/Modal window/script.js
+++ 04-Browser DOM Events/Modal window/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden'); //class name without dot
+  overlay.classList.remove('hidden');
+};
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+//console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//Esc pressed event
+document.addEventListener('keydown', function (event) {
+  console.log(event);
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
