c66003d1dc91141ff72d09600930fab582210eed
unknown <you@example.com>
2022-02-20 15:42:13 +0100

    Undercover. Done.

--- 05-Undercover/script.js
+++ 05-Undercover/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+
+  function printAge() {
+    if (birthYear > 1999) {
+      var isMillenial = true;
+    }
+    const output = `${firstName} ou are ${age}, born in ${birthYear}`;
+    console.log(isMillenial); //work - undefined
+    console.log(output);
+  }
+  printAge();
+  //console.log(isMillenial); //doesn't work
+  return age;
+}
+
+const firstName = 'Julia';
+calcAge(1998);
+//console.log(isMillenial); //doesn't work
