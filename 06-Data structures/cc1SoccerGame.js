e1277a0c1ad388c4ecb3cfe55d3b04ee1ebca930
unknown <you@example.com>
2022-02-23 09:54:01 +0100

    CC2. Soccer game. Done.

--- 06-Data structures/cc1SoccerGame.js
+++ 06-Data structures/cc1SoccerGame.js
@@ -1,4 +1,5 @@
 /*
+//CC1
 1. Create one player array for each team (variables 'players1' and 
 'players2')
 2. The first player in any player array is the goalkeeper and the others are field 
@@ -19,6 +20,8 @@ number of goals that were scored in total (number of player names passed in)
 team is more likely to win, without using an if/else statement or the ternary 
 operator.
 */
+
+//Given data
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -56,10 +59,12 @@ const game = {
   odds: {
     team1: 1.33,
     x: 3.25,
-    team2: 1.33,
+    team2: 6.5,
   },
 };
 
+/*
+//CC1 solution
 //1
 const [players1, players2] = game.players;
 console.log(players1);
@@ -93,3 +98,61 @@ printGoals(...game.scored);
 team1 < team2 && console.log(`Team 1 is more likely to win.`);
 team1 > team2 && console.log(`Team 2 is more likely to win.`);
 team1 === team2 && console.log(`They have same chances to win.`);
+*/
+
+//CC2
+/*
+1. Loop over the game.scored array and print each player name to the console, 
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already 
+studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them 
+(except for "draw"). Hint: Note how the odds and the game objects have the 
+same property names �
+4. Bonus: Create an object called 'scorers' which contains the names of the 
+players who scored as properties, and the number of goals as the value. In this 
+game, it will look like this:
+{
+ Gnarby: 1,
+ Hummels: 1,
+ Lewandowski: 2
+}
+*/
+
+//CC2 solution
+//1
+for (let plNumber = 0; plNumber < game.scored.length; plNumber++) {
+  console.log(`Goal ${plNumber + 1}: ${game.scored[plNumber]}`);
+}
+
+for (let [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+//2
+let sum = 0;
+const odds = Object.values(game.odds);
+for (let odd of odds) {
+  sum += odd;
+}
+console.log(sum / odds.length);
+
+//3
+let sum = 0;
+const odds = Object.entries(game.odds);
+for (let odd of odds) {
+  if (odd[0] === 'x') {
+    console.log(`Odd of draw: ${odd[1]}`);
+  } else {
+    console.log(`Odd of victory ${game[odd[0]]} : ${odd[1]}`);
+  }
+}
+//4
+let scorers = {};
+for (let scorer of game.scored) {
+  scorers[scorer] = !scorers[scorer] ? 1 : scorers[scorer] + 1;
+}
+console.log(scorers);
